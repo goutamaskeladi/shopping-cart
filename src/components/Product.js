861524b1d2b19670498cd257b140b7832c67bfda
Goutam A S <goutamas.keladi@gmail.com>
2019-10-13 19:31:03 +0530

    Displaying Product List

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class Product extends Component {
     render() {
+        const { id, discount, img_url, name, price } = this.props
         return (
-            <div>
-                <h2>Product</h2>
+            <div className="product">
+                <div className="product-image">
+                    <span className="discount">{discount}% off</span>
+                    <img src={img_url} alt={name}/>
+                </div>
+                <div className="product-info">
+                    <p className="product-name">{name}</p>
+                    <span className="product-price">${price}</span>
+                    <button className="add-to-cart-button">Add to cart</button>
+                </div>
             </div>
         )
     }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,21 +5,21 @@ import { ProductConsumer } from '../context/Products'
 export default class ProductList extends Component {
     render() {
         return (
-            <div>
+            <main>
                <ProductConsumer>
                    {
                        (context) => {
                           if(context.products.length > 0) {
                             return context.products.map(item => {
-                                return <Product key={item.id}/>
+                                return <Product key={item.id} {...item}/>
                             })
                           } else {
-                             return <h2>Loading, Please Wait...</h2>
+                             return <h2 className="loading-message">Loading, Please Wait...</h2>
                           }
                        }
                    }
                </ProductConsumer>
-            </div>
+            </main>
         )
     }
 }
